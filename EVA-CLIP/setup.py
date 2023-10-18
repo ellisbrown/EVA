fea22100cf66a24966ed7554db68e7d4335f6acb
Ellis Brown <ellis.l.brown.ii@gmail.com>
2023-10-17 21:32:56 -0400

    Create setup.py

---
+++ EVA-CLIP/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eva_clip',
+    version='0.1',
+    packages=find_packages(where='rei'),
+    package_dir={'': 'rei'},
+    install_requires=[
+        # You can automatically capture your dependencies from the requirements.txt file:
+        line.strip() for line in open('requirements.txt', 'r').readlines()
+    ],
+)
